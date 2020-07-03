48166b5cf10f9944273b0a3c4039c1cebd0f7cd2
wildenali <wildenwildenaliali@gmail.com>
2020-07-03 14:58:40 +0700

    first commit

---
+++ 05-MyPortofolio_v2-Basic/js/scriptTambahan/script.js
@@ -0,0 +1,63 @@
+// event pada saat link di click
+$('.halaman-scroll').on('click', function(e) {
+    
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // console.log(tujuan);
+
+    // tangkap element si dia
+    var elementTujuan = $(tujuan);
+    // console.log(elementTujuan);
+    // console.log(elementTujuan.offset().top);
+
+    // pindahkan scroll
+    $('html, body').animate({
+        scrollTop: elementTujuan.offset().top - 55
+    }, 1250, 'easeOutBounce');
+
+    e.preventDefault();
+
+});
+
+
+// parallax effect untuk About
+$(window).on('load', function() {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+// parallax effect
+$(window).scroll(function() {
+    var wScroll = $(this) .scrollTop();
+    // console.log(wScroll);
+
+    // jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/8 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/1.75 +'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll +'%)'
+    });
+
+
+    // portfolio
+    // if (wScroll > 1000) {
+    if (wScroll > $('.portfolio').offset().top - 250) {
+        // console.log('ok');
+        
+        $('.portfolio .thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            }, 1000 * (i+1));
+        });
+
+    }
+
+
+
+});
